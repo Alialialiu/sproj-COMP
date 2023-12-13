fc46b0f4aed985c29a2edc9fb3ff04cc5bd4be4f
quietus-ix <139018053+quietus-ix@users.noreply.github.com>
2023-12-13 22:51:05 +0800

    re-link all dependencies

--- dist/user/index.php
+++ dist/user/index.php
@@ -1,16 +1,19 @@
+<!-- 
+    CHANGES --
+        - restructure files
+        - relink all js and css dependencies of this code base
+ -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- custom -->
-    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="./style/index.css">
     <!-- bootstrap -->
-    <link href="bootstrap/animate.css" rel="stylesheet">
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- fonts -->
-    <script src="../bootstrap/jquery.js"></script>
+    <link href="../../src/bootstrap/animate.css" rel="stylesheet">
+    <link href="../../src/bootstrap/css/bootstrap.css" rel="stylesheet">
     <title>index</title>
 </head>
 <body>
@@ -37,10 +40,10 @@
         </div>
     </div>
 <!-- bootsrap -->
-<script src="jquery/index.js"></script>
-<script src="jquery/registration.js"></script>
-<script src="bootstrap/js/popper.js"></script>
-<script src="bootstrap/js/popper.min.js"></script>
-<script src="bootstrap/js/bootstrap.js"></script>
+<script src="../../src/jquery.js"></script>
+<script src="./jquery/index.js"></script>
+<script src="./jquery/registration.js"></script>
+<script src="../../src/popper.js"></script>
+<script src="../../src/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
